c810687de67edcf5d810c9b58f5c8901c0b2c350
Double778 <596638119@qq.com>
2016-06-12 16:11:49 +0800

    哥哥加了注释

--- TestForBashen/app/src/main/java/com/zhao/testforbashen/MainActivity.java
+++ TestForBashen/app/src/main/java/com/zhao/testforbashen/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // 新的循环开始了!
+
+        // 这是哥哥写的代码
     }
 }
